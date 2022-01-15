61a12f7d06f422f4de3fde57aa222a425bb89b0e
Averyyyyyyyy <92644634+Averyyyyyyyy@users.noreply.github.com>
2022-01-15 00:25:56 -0400

    feat: implemented randomInteger function

--- src/commands/roll.ts
+++ src/commands/roll.ts
@@ -1,5 +1,6 @@
 import { Client, Message } from "discord.js";
+import randomInteger from "../util/randomInteger";
 
-export default async function roll(client: Client, messasge: Message, args: string[]) {
-  console.log("roll dice");
+export default async function roll(client: Client, message: Message, args: string[]) {
+  message.reply({ content: `${randomInteger(1, 6)}` });
 }
